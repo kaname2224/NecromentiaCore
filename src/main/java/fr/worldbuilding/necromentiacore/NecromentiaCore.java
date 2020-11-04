2c5b13263e9532e1364033819035ac785813b659
Kaname <support@webcord.fr>
2020-11-04 14:38:37 +0100

    Initial commit

---
+++ src/main/java/fr/worldbuilding/necromentiacore/NecromentiaCore.java
@@ -0,0 +1,14 @@
+package fr.worldbuilding.necromentiacore;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NecromentiaCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+        Bukkit.getLogger().info("Plugin démarré");
+    }
+}
